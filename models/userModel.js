9fc7cf7f87883bfdb1b70b268f5e676e44631fbd
Sherwin Fernandes <66524582+SherwinRF@users.noreply.github.com>
2022-10-28 18:54:27 +0530

    Add files via upload

---
+++ models/userModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: [true, "user must have username"],
+        unique: true
+    },
+    password: {
+        type: String,
+        require: [true, "user must have password"]
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
